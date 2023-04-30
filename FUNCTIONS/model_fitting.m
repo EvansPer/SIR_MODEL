9881853016c0b4726dd715e8e71f6f18c5f1063e
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-04-30 12:36:10 +0200

    Add proper doc, modified fix to round function to reduce computational cost

--- FUNCTIONS/model_fitting.m
+++ FUNCTIONS/model_fitting.m
@@ -1,9 +1,19 @@
 function [delta,error] = model_fitting(active_infections,y,threshold)
+
+% This function provides a positive feedback (delta = true) when the threshold condition is met.
+% It is fed with the raw data vector "active_infections", the model
+% matrix y to be compared with and the threshold value. 
+
+% If the difference between the 2 vectors in more points is smaller than the
+% threshold value, than the condition is met.
+
+% The function provides the error of the model considering its distance
+% from the real data.
     
     delta = false;
-    if abs(active_infections(fix(end/2 - 10)) - y(fix(end/2 - 10),2)) <= threshold ...
-       && abs(active_infections(fix(end/2)) - y(fix(end/2),2)) <= threshold ...
-       && abs(active_infections(fix(end/2 + 10)) - y(fix(end/2 + 10),2)) <= threshold ...
+    if abs(active_infections(round(end/2 - 10)) - y(round(end/2 - 10),2)) <= threshold ...
+       && abs(active_infections(round(end/2)) - y(round(end/2),2)) <= threshold ...
+       && abs(active_infections(round(end/2 + 10)) - y(round(end/2 + 10),2)) <= threshold ...
        
         delta = true;
     end
