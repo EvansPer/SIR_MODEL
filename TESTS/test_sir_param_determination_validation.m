71590f44e01646e7cf6c016235bd5c70dc7521ff
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-04-30 18:13:05 +0200

    Adding test with doc

---
+++ TESTS/test_sir_param_determination_validation.m
@@ -0,0 +1,33 @@
+function test_sir_param_determination_validation()
+
+% This test verifies that, provided the true value of [t_true,y_true]
+% calculated with the function sir A_true and B_true, an error arises when the same
+% parameter are extracted by the function sir_param_determination and compared
+% with A_true and B_true. If the assert condition is false, then the test fails.
+
+% It is fed with:
+% - the 2 ODE parameters A_true and B_true
+% - time range tspan and Cauchy initial condition y0
+% - The simulated vector active_infections and a threshold
+
+% A and B are the vectors containing the possible parameters that
+% sir_param_determination may find.
+
+    A_true = 0.18;
+    B_true = 0.037;
+    tspan = [0, 161];
+    y0 = [1, 0.001, 0];
+    options = odeset("MaxStep",4.1);
+
+    [t_true, y_true] = ode45(@(t,y) sir(t,y,A_true,B_true), tspan, y0, options);
+    active_infections = y_true(:,2);
+    threshold = 0.007;
+    
+    A = 0:0.01:1;
+    B = 0:0.001:0.1;
+
+    [t, y] = sir_param_determination(A,B,tspan,y0,options,active_infections,threshold);
+    
+    assert(abs(A_true - A) < 1e-8 && abs(B_true - B) < 1e-8, "Test failed: function did not recover correct parameters");
+
+end
