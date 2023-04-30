32c249d5047d210e8dd62fede8f74738b078e2f8
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-04-30 12:45:16 +0200

    updated doc

--- FUNCTIONS/normalization.m
+++ FUNCTIONS/normalization.m
@@ -1,14 +1,18 @@
 function [new_positives,active_infections,total_removals,total_infected,deaths,recovers] = normalization(data)
 
 % This function provides the normilized variables extracted from the "data"
-% dataframe to be fitted with the ODE solutions. The best approach in this
-% situations is to scale down every variable to the normalized ODE solution
-% rather than scaling the former up, losing track of variables
-% interdependence.
+% dataframe to be fitted with the ODE solutions. 
+
+% It is fed with the dataframe "data" and returns the normalized vectors of
+% interest.
+
+% The best approach in this situations is to scale down every variable to the normalized ODE solution
+% rather than scaling the former up, losing track of variables interdependence.
+
 % The first 4 variables are defined as vectors from raw 1 to 161 of "data"
 % and coloumn 6,8,9,10, respectively.
 
-% new_positives = I(t) while 
+% new_positives = I(t) 
 % total removals = R(t) (ideally, so that a fit can be performed)
 % normalization_fac is the normalization factor provided by the maximum value of the total # of infected
 
