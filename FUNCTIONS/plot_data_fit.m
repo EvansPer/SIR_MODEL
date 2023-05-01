ba73fae2498675334a78ae7f8f0ba0ca3a0d4d4c
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-01 18:54:16 +0200

    added other graphs and folder destinations

--- FUNCTIONS/plot_data_fit.m
+++ FUNCTIONS/plot_data_fit.m
@@ -1,4 +1,4 @@
-function plot_data_fit(time,t,y,model_D_vs_R,model_D,model_recovered,model_total_infected)
+function plot_data_fit(time,t,y,model_D_vs_R,model_D,model_recovered,model_total_infected,R_e)
 
     load("Norm_dataset.mat","total_removals","new_positives","active_infections","deaths","recovers","total_infected","time");
     
@@ -27,38 +27,51 @@ function plot_data_fit(time,t,y,model_D_vs_R,model_D,model_recovered,model_total
     plot(t,model_total_infected,"--","LineWidth",1); hold off;
     legend("Total infected","SIR I_T(t)","Location","east");
     grid on;
-
-
-
-%     plot(t,y);
-%     legend("Susceptibles","Infeted","Removed");
-%     
-%     R_e = y(:,1).*a/b;
-%     figure;
-%     plot(t,R_e,"o");
-%     legend("R_e");  
-%     
-%     
-%     figure;
-%     plot(time,new_positives,"+");
-%     legend("New pos"); 
     
+    if ~exist("GRAPHS","dir")
+    mkdir("GRAPHS");
+    end
+
+    data_vs_model = fullfile("GRAPHS","data_vs_model.jpg");
+    saveas(gcf, data_vs_model);
+   
+
+    figure;
+    plot(t,y,"LineWidth",1);
+    legend("Susceptibles","Infeted","Removed","Location","east");
+    grid on;
+
+    SIR_model = fullfile("GRAPHS","SIR_model.jpg");
+    saveas(gcf, SIR_model);
+
     
-%     
-%     figure;
-%     plot(total_removals,deaths,"*");
-%     hold on;
-%     plot(model_D_vs_R);
-%     
-%     hold off;
-%     
-%     
-%     
-%     figure;
-%     plot(time,deaths,"diamond"); hold on;
-%     plot(t,model_D,"--");
-%     hold off;
-%     legend("Deaths","Fit D(R_m)");  
+    figure;
+    plot(t,R_e,"o","LineWidth",1);
+    legend("Reproductive rate","Location","east");  
+    grid on;
+
+    R_e = fullfile("GRAPHS","R_e.jpg");
+    saveas(gcf, R_e);
     
-  
+
+    figure;
+
+    subplot(1,2,1);
+    plot(total_removals,deaths,"*","LineWidth",1);
+    hold on;
+    plot(model_D_vs_R);
+    hold off;
+    legend("Raw data","Fit model","Location","east")
+    grid on;
+    
+    subplot(1,2,2);
+    plot(time,deaths,"diamond","LineWidth",1); hold on;
+    plot(t,model_D,"--","LineWidth",1);
+    hold off;
+    legend("Deaths","Fit D(R_m)","Location","east");  
+    grid on;
+    
+    model_R_plot = fullfile("GRAPHS","model_R_plot.jpg");
+    saveas(gcf, model_R_plot);
+
 end
