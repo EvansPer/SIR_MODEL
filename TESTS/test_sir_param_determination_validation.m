8bd7049d0f29ca10ed64f14a0130545eb7609132
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-01 10:26:00 +0200

    modified y0 to reach normalization, updated doc, modified last assertion, invalid parameters deployed

--- TESTS/test_sir_param_determination_validation.m
+++ TESTS/test_sir_param_determination_validation.m
@@ -3,7 +3,8 @@ function test_sir_param_determination_validation()
 % This test verifies that, provided the true value of [t_true,y_true]
 % calculated with the function sir A_true and B_true, an error arises when the same
 % parameter are extracted by the function sir_param_determination and compared
-% with A_true and B_true. If the assert condition is false, then the test fails.
+% with A_true and B_true. If the assert condition (which compares the value of A_true and A,
+% B_true and B with a threshold of 1e-8) is false, then the test fails.
 
 % It is fed with:
 % - the 2 ODE parameters A_true and B_true
@@ -16,7 +17,7 @@ function test_sir_param_determination_validation()
     A_true = 0.18;
     B_true = 0.037;
     tspan = [0, 161];
-    y0 = [1, 0.001, 0];
+    y0 = [0.999, 0.001, 0];
     options = odeset("MaxStep",4.1);
 
     [t_true, y_true] = ode45(@(t,y) sir(t,y,A_true,B_true), tspan, y0, options);
@@ -28,6 +29,6 @@ function test_sir_param_determination_validation()
 
     [t, y] = sir_param_determination(A,B,tspan,y0,options,active_infections,threshold);
     
-    assert(abs(A_true - A) < 1e-8 && abs(B_true - B) < 1e-8, "Test failed: function did not recover correct parameters");
+    assert(abs(A_true - A(end)) < 1e-8 && abs(B_true - B(end)) < 1e-8, "Test failed: function did not recover correct parameters");
 
 end
