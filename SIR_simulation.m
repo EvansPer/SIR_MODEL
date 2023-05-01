c23be43cc560b09d6bc262ed4be2b59a47556742
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-01 19:06:15 +0200

    Modified variable names A, B. Added reproductive rate variable R_e. Added plot function plot_data_fit

--- SIR_simulation.m
+++ SIR_simulation.m
@@ -2,78 +2,23 @@ clc; close all; clearvars;
 
 %% IMPORTING DATA
 
-load("dataframe.mat");
+load("dataframe.mat","data","time","tspan","y0","options","A","B","threshold");
 
 addpath(genpath("Functions"));
-addpath(genpath("Plots"));
+addpath(genpath("GRAPHS"));
 addpath(genpath("Tests"));
 
 [new_positives,active_infections,total_removals,total_infected,deaths,recovers] = normalization(data);
 
 %% SIR MODEL
 
-[t,y] = sir_param_determination(a,b,tspan,y0,options,active_infections,threshold);
+[t,y] = sir_param_determination(A,B,tspan,y0,options,active_infections,threshold);
 
+R_e = y(:,1).*A/B;
 
-%%
-plot(t,y);
-legend("Susceptibles","Infeted","Removed");
-
-%% R_e
-R_e = y(:,1).*a/b;
-figure;
-plot(t,R_e,"o");
-legend("R_e");
-
-%% ACTIVE INFECTIONS
-
-figure;
-plot(time,active_infections,"*"); hold on;
-plot(t,y(:,2),"--");
-legend("Active infections","Model");
-
-%% NEW POS
-
-figure;
-plot(time,new_positives,"+");
-legend("New pos");
-
-%% TOTAL R
-
-figure;
-plot(time,total_removals,"."); hold on;
-plot(t,y(:,3),"--");
-legend("Removals","Model R(t)");
-
-%% D vs R
 [model_D_vs_R,model_D, model_recovered] = model_R(y,total_removals,deaths);
-figure;
-plot(total_removals,deaths,"*");
-hold on;
-plot(model_D_vs_R);
 
-hold off;
-
-%% DEATHS
-
-figure;
-plot(time,deaths,"diamond"); hold on;
-plot(t,model_D,"--");
-hold off;
-legend("Deaths","Fit D(R_m)");
-
-%%  RECOVERIES
-
-figure;
-plot(time,recovers,"o"); hold on;
-plot(t,model_recovered,"--");
-legend("Recevers","Model R(t)");
-
-%%
 model_total_infected = (y(:,3) + y(:,2));
 
-figure;
-plot(time,total_infected,"Marker","hexagram"); hold on;
-plot(t,model_total_infected,"--");
-hold off;
-legend("Total infected","Model I_T(t)");
+plot_data_fit(time,t,y,model_D_vs_R,model_D,model_recovered,model_total_infected,R_e);
+
