0064269ef565b96bba55ff5b4c7ac60cbbe93b21
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-01 11:30:31 +0200

    modified assert of equality with threshold, assert of y(:,2) with index of max value of y, updated doc

--- TESTS/test_sir_time_profile.m
+++ TESTS/test_sir_time_profile.m
@@ -5,22 +5,23 @@ function test_sir_time_profile()
 
 % 1) The total number of people must be normalized to 1
 % 2) The number of susceptibles decreases over time
-% 3) The number of infecteds increases and then decreases over time
+% 3) The number of infecteds increases up to its max value and then decreases over time
 % 4) The number of recovered individuals increases over time
 
-% Test 1, A = 0.3, B = 0.1
-    [t, y] = ode45(@(t,y) sir(t,y,0.3,0.1), [0, 10], [0.999, 0.001, 0]);
-    
-    assert(all(sum(y, 2) == 1));
-    assert(all(diff(y(:,1)) < 0));
-    assert(all(diff(y(:,2)) > 0) & all(diff(y(:,2)) < 0));
-    assert(all(diff(y(:,3)) > 0));
+% Test 1, A = 0.18, B = 0.037
+    [t, y] = ode45(@(t,y) sir(t,y,0.18,0.037), [0, 150], [0.999, 0.001, 0]);
+    [i,j] = max(y(:,2));
+
+    assert(all(sum(y, 2) - 1 <= 1e-15));
+    assert(all(diff(y(:,1)) <= 0));
+    assert(all(diff(y(1:j,2)) >= 0) & all(diff(y(j:end,2)) <= 0));
+    assert(all(diff(y(:,3)) >= 0));
 
 % Test 2, A = 0.4, B = 0.2
-    [t, y] = ode45(@(t,y) sir(t,y,0.4,0.2), [0, 10], [0.999, 0.001, 0]);
-    
-    assert(all(sum(y, 2) == 1));
-    assert(all(diff(y(:,1)) < 0));
-    assert(all(diff(y(:,2)) > 0) & all(diff(y(:,2)) < 0));
-    assert(all(diff(y(:,3)) > 0));
+    [t, y] = ode45(@(t,y) sir(t,y,0.4,0.2), [0, 150], [0.999, 0.001, 0]);
+    [i,j] = max(y(:,2));
+    assert(all(sum(y, 2) - 1 <= 1e-15));
+    assert(all(diff(y(:,1)) <= 0));
+    assert(all(diff(y(1:j,2)) >= 0) & all(diff(y(j:end,2)) <= 0));
+    assert(all(diff(y(:,3)) >= 0));
 end
