dcf56b9f8a68a0899825f5199be67da03d7e8e8a
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-20 18:02:43 +0200

    Added duration parameter. Added assertion for all variables

--- TESTS/test_normalization_zero_data.m
+++ TESTS/test_normalization_zero_data.m
@@ -5,11 +5,17 @@ function test_normalization_zero_data()
 % be performed by dividing by normalization_fac = max(total_infected) = 0.
 
 % It is fed with a dataset of 161 raws of zeros and 10 columns
-
-    data = zeros(161, 10);
-    [new_positives, active_infections, total_removals, total_infected, deaths, recovers] = normalization(data);
+    duration = 161;
+    data = zeros(duration, 10);
+    [new_positives, active_infections, total_removals, total_infected, deaths, recovers] = normalization(data,duration);
     
     assert(all(total_infected == zeros(161, 1)),"Error: provided dataset of zeros, can't divide by 0");
+    assert(all(new_positives == zeros(161, 1)),"Error: provided dataset of zeros, new_positives is empty");
+    assert(all(active_infections == zeros(161, 1)),"Error: provided dataset of zeros, active_infections is empty");
+    assert(all(total_removals == zeros(161, 1)),"Error: provided dataset of zeros, total_removals is empty");
+    assert(all(deaths == zeros(161, 1)),"Error: provided dataset of zeros, deaths is empty");
+    assert(all(recovers == zeros(161, 1)),"Error: provided dataset of zeros, recovers is empty");
+   
     
     
 end
