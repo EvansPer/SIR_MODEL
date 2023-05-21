8d4a03e7536a44aecccf1ec34ae2e4dd5b7ebe44
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-21 18:42:41 +0200

    Updated doc

--- TESTS/test_normalization_zero_data.m
+++ TESTS/test_normalization_zero_data.m
@@ -1,23 +1,22 @@
 function test_normalization_zero_data()
 
-% This test verifies that, provided a dataset of zeros, the function normalization can handle
-% the dataframe as well, but an error arises since the normalization should
+% This test verifies that, provided a dataset of zeros, the function normalization can't handle
+% the dataframe as well, and an error arises since the normalization should
 % be performed by dividing by normalization_fac = max(total_infected) = 0.
 
 % It is fed with a dataset of 161 raws of zeros and 10 columns
+
     duration = 161;
     data = zeros(duration, 10);
     [new_positives, active_infections, total_removals, total_infected, deaths, recovers] = normalization(data,duration);
-    
+     
     assert(all(total_infected == zeros(duration, 1)),"Error: provided dataset of zeros, can't divide by 0");
     assert(all(new_positives == zeros(duration, 1)),"Error: provided dataset of zeros, new_positives is empty");
     assert(all(active_infections == zeros(duration, 1)),"Error: provided dataset of zeros, active_infections is empty");
     assert(all(total_removals == zeros(duration, 1)),"Error: provided dataset of zeros, total_removals is empty");
     assert(all(deaths == zeros(duration, 1)),"Error: provided dataset of zeros, deaths is empty");
     assert(all(recovers == zeros(duration, 1)),"Error: provided dataset of zeros, recovers is empty");
-   
-    
-    
+  
 end
 
    
