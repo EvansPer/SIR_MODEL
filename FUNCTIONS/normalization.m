01c61bce1c0e248ef9fad371aa1826a49d5ccf05
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-22 20:32:48 +0200

    Updated doc: added info on input and output parameters, improved info layout

--- FUNCTIONS/normalization.m
+++ FUNCTIONS/normalization.m
@@ -1,25 +1,32 @@
 function [new_positives,active_infections,total_removals,total_infected,deaths,recovers] = normalization(data,duration)
 
 % This function provides the normilized variables extracted from the "data"
-% dataframe to be fitted with the ODE solutions. 
+% dataframe (by means of the value normalization_fact) to be fitted with the ODE solutions. 
 
-% It is fed with the dataframe "data" (matrix containing the raw data of the virus evolution) and returns the normalized vectors of
-% interest.
+% INPUT PARAMETERS:
+% - data: nx10 matrix (n generic) containing the raw data of the virus evolution
+% - duration: positive number determining the time span to analyse 
+
+% OUTPUT PARAMETERS:
+% - new_positives: duration x 1 column vector normalized to 1 -> I(t) of the SIR model
+%   containing the data of the column 8 of "data"
+% - active_infections: duration x 1 column vector normalized to 1
+%   containing the data of the column 6 of "data"
+% - total_removals: duration x 1 column vector normalized to 1 -> R(t) of the SIR model
+%   given by the sum of recovers and deaths
+% - total_infected: duration x 1 column vector normalized to 1
+%   given by the sum of total_removals and active_infections
+% - deaths: duration x 1 column vector normalized to 1
+%   containing the data of the column 10 of "data"
+% - recovers: duration x 1 column vector normalized to 1
+%   containing the data of the column 9 of "data"
+% A dataframe called "Norm_dataset.mat" is generated
 
 % The best approach in this situations is to scale down every variable to the normalized ODE solution
 % rather than scaling the former up, losing track of variables interdependence.
 
-% The first 4 variables are defined as vectors from raw 1 to duration = 161 (thus, from 02/26 to 08/01/2020 of "data"
-% and coloumn 6,8,9,10, respectively.
-
-% new_positives = I(t) 
-% total removals = R(t) (ideally, so that a fit can be performed)
 % normalization_fac is the normalization factor provided by the maximum value of the total # of infected
 
-% Once all the variables have been defined, along with the
-% normalization_fac, it is possible to scale them down to max 1.
-
-% Finally, it saves the generated normalized vectors in a dataframe calle "Norm_dataset.mat"
 
     active_infections = data(1:duration,6);
     new_positives = data(1:duration,8); % I(t) in the ODE
