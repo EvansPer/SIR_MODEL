336d792d5e0275f04fd568f004f9beea3ebecc90
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-22 19:29:49 +0200

    Updating doc: added info on input and output parameters, improved info layout

--- FUNCTIONS/distance_model_raw.m
+++ FUNCTIONS/distance_model_raw.m
@@ -3,14 +3,19 @@ function [delta,error] = distance_model_raw(active_infections,y,threshold)
 % This function provides a positive feedback (delta = true) when the threshold condition is met, by 
 % means of the boolean flag "delta".
 
-% It is fed with the raw data vector "active_infections", the model
-% matrix y to be compared with and the threshold value. 
+% INPUT PARAMETERS:
+% - active_infections: nx1 column vector (n generic) 
+% - y: nx3 matrix, solution of the ODE system of the SIR model
+% - threshold: float positive number (if too small, convergence may not be found)
 
-% If the difference between the 2 vectors in more points is smaller than the
-% threshold value, than the condition is met.
+% OUTPUT PARAMETERS:
+% - delta: boolean 1x1 array, may be true (logical(1)) or false (logical(0))
+% - error: float positive number, measuring the distance between one point
+%   of the active_infections vector and the correspondent value of y.
 
-% The function provides the error of the model considering its distance
-% from the real data.
+% If the difference between active_infections and y(:,2) (which is the vector of the model corresponding
+% to active_infections) in more points is smaller than the threshold value, than the condition is met 
+% (delta = true) and the error value is computed.
     
     delta = false;
     if abs(active_infections(round(end/2 - 10)) - y(round(end/2 - 10),2)) <= threshold ...
