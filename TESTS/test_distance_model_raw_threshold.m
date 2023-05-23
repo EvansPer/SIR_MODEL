5b804ebe34a06927ce13100baa377db5ac27f3d6
Evans Pericoli <evans.pericoli@studio.unibo.it>
2023-05-23 12:07:00 +0200

    Updated doc: added info on init parameters and improved info structure

--- TESTS/test_distance_model_raw_threshold.m
+++ TESTS/test_distance_model_raw_threshold.m
@@ -1,13 +1,19 @@
 function test_distance_model_raw_threshold()
 
 % This test verifies that, provided with vectors with close values but
-% a threshold too small, the function model_fitting returns a delta = false,
+% a threshold too small, the function distance_model_raw returns a delta = false,
 % since the values are not sufficiently close.
 
-% It is fed with 2 column vectors active_infections and y and a threshold
-% value.
+% INIT PARAMETERS:
+% - active_infections = nx1 (n generic) column vector with numbers in
+%   between 100 and 150
+% - a: nx1 column vector with random numbers oscillating on the mean
+%   defined by the vector 100:150
+% - y = nx2 (n generic) matrix (y(:,2) = a will be compared by means of
+%   distance_model_raw)
+% - threshold: float positive number needed for vector comparison 
 
-% The error arises since the threshold is too small
+% The error arises when delta is still true despite the small threshold
 
     addpath(genpath(fullfile(pwd,"..","FUNCTIONS")));
     
